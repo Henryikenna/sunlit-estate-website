eb1d3f05a4a32f9d89cf569f1e8d90d922c99336
henryoncode <ikennaunegbu10@gmail.com>
2024-06-23 04:49:17 +0100

    eighteenth commit

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -97,15 +97,26 @@ const ListingDetails = ({
   // // const searchParams = useSearchParams()
   //   const param1 = router.query.param1;
   //   const param2 = searchParams.getAll('param2')
-  const [currentSlide, setCurrentSlide] = useState(0)
+  
+  // const [currentSlide, setCurrentSlide] = useState(0)
+
+  // const handlePrev = () => {
+  //   setCurrentSlide((prev) => (prev > 0 ? prev - 1 : searchParams.images.length - 1))
+  // }
+
+  // const handleNext = () => {
+  //   setCurrentSlide((prev) => (prev < searchParams.images.length - 1 ? prev + 1 : 0))
+  // }
+
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   const handlePrev = () => {
-    setCurrentSlide((prev) => (prev > 0 ? prev - 1 : searchParams.images.length - 1))
-  }
+    setCurrentSlide((prev) => (prev > 0 ? prev - 1 : searchParams.images.length - 1));
+  };
 
   const handleNext = () => {
-    setCurrentSlide((prev) => (prev < searchParams.images.length - 1 ? prev + 1 : 0))
-  }
+    setCurrentSlide((prev) => (prev < searchParams.images.length - 1 ? prev + 1 : 0));
+  };
 
   const [selectedPage, setSelectedPage] = useState('description')
 
@@ -115,9 +126,8 @@ const ListingDetails = ({
 
   return (
     <div className='pt-[88px] px-3 md:px-10 lg:px-[3.75rem]'>
-      <section className='flex flex-col-reverse gap-3 pt-6 md:gap-6 md:pt-10 md:flex-row'>
+      {/* <section className='flex flex-col-reverse gap-3 pt-6 md:gap-6 md:pt-10 md:flex-row'>
         <div className='flex flex-row gap-2 md:flex-col'>
-          {/* {searchParams.images.map(({image})) } */}
           {searchParams.images.map((imageUrl, index) => (
             <img key={index} className='h-14 w-14 object-cover rounded-lg' src={imageUrl} alt={`Image ${index}`} />
           ))}
@@ -127,45 +137,6 @@ const ListingDetails = ({
           {searchParams.images.map((image, index) => (
             <div key={`slide-${index}`} className={`carousel-item relative w-full ${index === currentSlide ? 'block' : 'hidden'}`}>
               <img src={image} className='w-full h-[200px] rounded-2xl object-cover md:h-[400px] lg:h-[495px]' />
-              {/* <div className='absolute top-0 left-0 right-0 flex justify-between items-center content-center w-auto space-x-2 pt-5 px-4'>
-            <section className=' flex gap-1 md:gap-2'>
-              <button className=' shadow-sm text-white text-[0.4375rem] bg-black bg-opacity-70 w-[0.9625rem] h-[0.9625rem] flex justify-center items-center rounded-full md:text-[0.8125rem] md:w-[1.625rem] md:h-[1.625rem]'>
-
-                <IoBedOutline />
-              </button>
-              <button className=' shadow-sm text-white text-[0.4375rem] bg-black bg-opacity-70 w-[0.9625rem] h-[0.9625rem] flex justify-center items-center rounded-full md:text-[0.8125rem] md:w-[1.625rem] md:h-[1.625rem] transform scale-x-[-1]'>
-            
-                <LiaBathSolid />
-              </button>
-              <button className=' shadow-sm text-white text-[0.4375rem] bg-black bg-opacity-70 w-[0.9625rem] h-[0.9625rem] flex justify-center items-center rounded-full md:text-[0.8125rem] md:w-[1.625rem] md:h-[1.625rem] transform scale-x-[-1]'>
-               
-                <LiaSwimmingPoolSolid />
-              </button>
-              <button className=' shadow-sm text-white text-[0.4375rem] bg-black bg-opacity-70 w-[0.9625rem] h-[0.9625rem] flex justify-center items-center rounded-full md:text-[0.8125rem] md:w-[1.625rem] md:h-[1.625rem]'>
-               
-                <FaCheck />
-              </button>
-            </section>
-
-            <section className=''>
-              <svg
-                className={`w-[0.8025rem] h-[0.8025rem] md:w-6 md:h-6 ${isLikeButtonAnimating ? 'pop-animation' : ''}`}
-                viewBox='0 0 24 23'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-                onClick={likeButtonFill}
-                onAnimationEnd={handleLikeButtonAnimationEnd}
-                style={{ cursor: 'pointer' }}
-              >
-                <path
-                  d='M11.6416 2.69307L12 3.06148L12.3584 2.69307C15.4196 -0.453634 20.8456 0.7004 22.7895 4.49668C23.745 6.36253 23.8732 8.91857 22.3206 11.9819C20.7865 15.0084 17.6125 18.52 12 22.2736C6.38744 18.5204 3.21347 15.009 1.67944 11.9825C0.126771 8.91927 0.255045 6.36318 1.21049 4.49723C3.15447 0.700726 8.58047 -0.453596 11.6416 2.69307Z'
-                  fill={isLiked ? '#F6812D' : '#1E1E1E'}
-                  fillOpacity={isLiked ? '1' : '0.4'}
-                  stroke='white'
-                />
-              </svg>
-            </section>
-          </div> */}
               <div className={`absolute justify-between transform -translate-y-1/2 left-0 right-0 w-auto px-[0.375rem] top-1/2 lg:px-3 ${searchParams.images.length === 1 ? 'hidden' : 'flex'}`}>
                 {index > 0 ? (
                   <button className='btn btn-circle btn-xs bg-[#1E1E1E] bg-opacity-60 text-white outline-none border-none lg:btn-sm' onClick={handlePrev}>
@@ -182,15 +153,62 @@ const ListingDetails = ({
                   <button></button>
                 )}
               </div>
-              {/* <div className={`absolute bottom-0 left-0 right-0 flex justify-center items-center content-center w-auto space-x-2 p-4 ${images.length === 1 ? 'hidden' : 'flex'}`}>
-            {searchParams.images.slice(0, 5).map((_, dotIndex) => (
-              <span key={dotIndex} className={`inline-block rounded-full h-1 w-1 lg:h-[0.4375rem] lg:w-[0.4375rem] ${dotIndex === currentSlide ? 'bg-white' : 'bg-gray-400'}`}></span>
-            ))}
-          </div> */}
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
+      <section className='flex flex-col-reverse gap-3 pt-6 md:gap-6 md:pt-10 md:flex-row'>
+      <div className='flex flex-row gap-2 md:flex-col'>
+        {searchParams.images.map((imageUrl, index) => (
+          <div className="relative">
+            <img
+            key={index}
+            className='h-14 w-14 object-cover rounded-lg'
+            src={imageUrl}
+            alt={`Image ${index}`}
+          />
+          <div className={`${index === currentSlide ? '' : 'absolute inset-0 bg-white bg-opacity-50'}`}></div>
+          </div>
+        ))}
+      </div>
+
+      <div className='carousel w-full'>
+        {searchParams.images.map((image, index) => (
+          <div
+            key={`slide-${index}`}
+            className={`carousel-item relative w-full ${index === currentSlide ? 'block' : 'hidden'}`}
+          >
+            <img src={image} className='w-full h-[200px] rounded-2xl object-cover md:h-[400px] lg:h-[495px]' />
+            <div
+              className={`absolute justify-between transform -translate-y-1/2 left-0 right-0 w-auto px-[0.375rem] top-1/2 lg:px-3 ${
+                searchParams.images.length === 1 ? 'hidden' : 'flex'
+              }`}
+            >
+              {index > 0 ? (
+                <button
+                  className='btn btn-circle btn-xs bg-[#1E1E1E] bg-opacity-60 text-white outline-none border-none lg:btn-sm'
+                  onClick={handlePrev}
+                >
+                  ❮
+                </button>
+              ) : (
+                <button></button>
+              )}
+              {index < searchParams.images.length - 1 ? (
+                <button
+                  className='btn btn-circle btn-xs bg-[#1E1E1E] bg-opacity-60 text-white outline-none border-none lg:btn-sm'
+                  onClick={handleNext}
+                >
+                  ❯
+                </button>
+              ) : (
+                <button></button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
 
       <section className='pt-3 block justify-between md:pt-4 md:flex'>
         <h3 className='text-[#1E1E1EE5] text-2xl pb-2 font-openSans font-semibold md:pb-0 md:text-3xl'>{searchParams.address}</h3>
@@ -344,7 +362,7 @@ const ListingDetails = ({
         </div>
 
         {/* <div className=""> */}
-        <img className='w-full h-96 object-cover rounded-2xl lg:h-[530px] lg:w-3/5' src='https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8fDA%3D' alt='' />
+        <img className='w-full h-60 object-cover rounded-2xl lg:h-[400px] lg:w-3/5' src='https://images.unsplash.com/photo-1570129477492-45c003edd2be?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2V8ZW58MHx8MHx8fDA%3D' alt='' />
         {/* </div> */}
       </section>
 
